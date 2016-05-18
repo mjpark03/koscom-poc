ee67c413d16417b3fd2e9401333c95f5afa9f03c
mjpark03 <mjpark03@gmail.com>
2016-05-18 22:17:30 +0900

    Add functions for using scalechain apis

---
+++ config/user.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Rachel on 2016. 5. 16..
+ */
+
+var user = {
+
+    issuer : {
+        account_id : '3p83M65C2pXyZbP7b',
+        to_address : 'bXAujEs8TUu1XafL6MrZTiB1R22cyYempRg',
+        change_address : 'bX9cdGsVAC9hntzNxtJwvwL33nhB9ft7rKr',
+        asset_id : '',
+        amount : 0
+    },
+
+    receiver : {
+        account_id : 'fJn29fR9PyGc5QiwJ',
+        to_address : 'bX9cdGsVAC9hntzNxtJwvwL33nhB9ft7rKr',
+        change_address : 'bXAujEs8TUu1XafL6MrZTiB1R22cyYempRg',
+        asset_id : 'AHhucsmtFpk1FyVVtpNpqkHaKUCi7My9c6',
+        amount : 0
+    },
+
+    issuerName : '손새미',
+    receiverName : '이강욱',
+    count : 0
+}
+
+module.exports = user;
