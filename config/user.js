ed3fcf46a08ba749a0db8a28e4d33cd1c0975ae7
mjpark03 <mjpark03@gmail.com>
2016-05-20 12:21:35 +0900

    Modify user info

--- config/user.js
+++ config/user.js
@@ -5,18 +5,18 @@
 var user = {
 
     issuer : {
-        account_id : '3p83M65C2pXyZbP7b',
-        to_address : 'bXAujEs8TUu1XafL6MrZTiB1R22cyYempRg',
-        change_address : 'bX9cdGsVAC9hntzNxtJwvwL33nhB9ft7rKr',
+        account_id : 'NSFsid9xD5Yp6gjB2',
+        to_address : 'bWrbUoptRkRfjC9ehSrp1zRqg4PhJtr1T5c',
+        change_address : 'bX2nKUZByd4HMt5FVJSV6tin4pZZmunnapi',
         asset_id : '',
         amount : 0
     },
 
     receiver : {
-        account_id : 'fJn29fR9PyGc5QiwJ',
-        to_address : 'bX9cdGsVAC9hntzNxtJwvwL33nhB9ft7rKr',
-        change_address : 'bXAujEs8TUu1XafL6MrZTiB1R22cyYempRg',
-        asset_id : 'AHhucsmtFpk1FyVVtpNpqkHaKUCi7My9c6',
+        account_id : 'YYdm6JBQZ4HBRZ5ws',
+        to_address : 'bX2nKUZByd4HMt5FVJSV6tin4pZZmunnapi',
+        change_address : 'bWrbUoptRkRfjC9ehSrp1zRqg4PhJtr1T5c',
+        asset_id : 'AFz6BRgwfv1C4N7osnLWSJHDREqzw6MpFA',
         amount : 0
     },
 
